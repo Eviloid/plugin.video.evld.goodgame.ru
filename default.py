55a1451929e97c5a032e65f094d6f6604e146439
Eviloid <eviloid@gmail.com>
2019-11-24 16:59:10 +0300

    authorization and favorites

--- default.py
+++ default.py
@@ -21,27 +21,56 @@ addon = xbmcaddon.Addon(id='plugin.video.evld.goodgame.ru')
 Pdir = addon.getAddonInfo('path')
 icon = xbmc.translatePath(os.path.join(Pdir, 'icon.png'))
 fanart = xbmc.translatePath(os.path.join(Pdir, 'fanart.jpg'))
+fcookies = xbmc.translatePath(os.path.join(Pdir, 'cookies.txt'))
+cj = cookielib.MozillaCookieJar(fcookies)
+
 
 xbmcplugin.setContent(handle, 'videos')
 
 def get_html(url, params={}, post={}, noerror=True):
     headers = {'Accept':'application/json'}
 
+    if post:
+        headers['Content-Type'] = 'application/x-www-form-urlencoded'
+        req = urllib2.Request('%s' % url, urllib.urlencode(post), headers=headers)
+    else:
+        req = urllib2.Request('%s?%s' % (url, urllib.urlencode(params)), headers=headers)
+
     html = ''
 
     try:
-        conn = urllib2.urlopen(urllib2.Request('%s?%s' % (url, urllib.urlencode(params)), headers=headers))
+        conn = urllib2.urlopen(req)
         html = conn.read()
         conn.close()
     except urllib2.HTTPError, err:
         if not noerror:
             html = err.code
 
-    return html 
+    return html
+
+
+def do_login():
+    if addon.getSetting('User'):
+        post = {'login':addon.getSetting('User'), 'password':addon.getSetting('Password'), 'remember':1}
+        get_html('https://goodgame.ru/api/4/login', post=post)
+
+        cj.save(fcookies, True, True)
+
+	xbmc.executebuiltin('Container.Refresh')
+
+
+def checkauth():
+    html = get_html('https://goodgame.ru/api/4/favorites', noerror=False)
+    if isinstance(html, basestring):
+        data = json.loads(html)
+
+        if isinstance(data, list):
+            return True
+    return False
 
 
 def list_streams(params):
-    data = get_html('https://api2.goodgame.ru/v2/streams', {'only_gg':'1','page':params['page'],'ids':params.get('ids', '')}, noerror=False)
+    data = get_html('https://api2.goodgame.ru/v2/streams', {'only_gg':'1','page':params['page'],'ids':params.get('ids', ''),'game':params.get('game', '')}, noerror=False)
 
     if not isinstance(data, basestring):
         if params['page'] > 0:
@@ -52,7 +81,7 @@ def list_streams(params):
         for s in data['_embedded']['streams']:
             channel = s['channel']
             title = '%s. %s' % (channel['key'], channel['title'])
-            preview = 'http:' + channel['thumb']
+            preview = 'https:' + channel['thumb']
 
             plot = common.replaceHTMLCodes(channel['description'])
             plot = plot.replace('<br>', '\n')
@@ -76,7 +105,13 @@ def list_streams(params):
 
             if s['status'] == 'Dead':
                 title = '[COLOR red]%s[/COLOR]' % title
-
+                if '.jpg' in channel['img']:
+                    preview = channel['img'].replace('.jpg', '_orig.jpg')
+                elif '.png' in channel['img']:
+                    preview = channel['img'].replace('.png', '_orig.png')
+                else:
+                    preview = 'https://goodgame.ru/images/stream-offline.png'
+            
             add_item(title, {'mode':'play', 'url':'https://hls.goodgame.ru/hls/' + player_id}, icon=preview, poster=preview, fanart=fanart, plot=plot, isPlayable=True)
 
         if data['page_count'] > data['page']:
@@ -86,17 +121,25 @@ def list_streams(params):
 
 
 def list_favorites(params):
-    ids = addon.getSetting('Favorites')
-    if ids:
-        params['ids'] = ids
-        list_streams(params)
+
+    html = get_html('https://goodgame.ru/api/4/favorites', noerror=False)
+    if isinstance(html, basestring):
+        data = json.loads(html)
+        ids = ''
+
+        if isinstance(data, list):
+            for f in data:
+                ids = '%s,%s' % (ids, f['channelkey'])
+            params['ids'] = ids
+
+            list_streams(params)
 
     xbmcplugin.endOfDirectory(handle, cacheToDisc=False)
 
 
 def main_menu(params):
 
-    if addon.getSetting('EnableFavorites') == 'true' and params['page'] == 1:
+    if checkauth() and params['page'] == 1:
         add_nav(u'[B]Избранные стримы[/B]', {'mode':'favorites'})
 
     list_streams(params)
@@ -149,17 +192,30 @@ def add_item(title, params={}, icon='', banner='', fanart='', poster='', thumb='
     xbmcplugin.addDirectoryItem(handle, url=url, listitem=item, isFolder=isFolder)
 
 
+try:
+    cj.load(fcookies, True, True)
+except:
+    pass
+
+hr = urllib2.HTTPCookieProcessor(cj)
+opener = urllib2.build_opener(hr)
+urllib2.install_opener(opener)
+
+
 params = common.getParameters(sys.argv[2])
 params['mode'] = mode = params.get('mode', 'list')
 params['page'] = params.get('page', 1)
 
-if mode == 'list':
+if mode == 'login':
+    do_login()
+
+elif mode == 'list':
     tcc().remove_like(LIVE_PREVIEW_TEMPLATE, False)
     main_menu(params)
 
-if mode == 'favorites':
+elif mode == 'favorites':
     tcc().remove_like(LIVE_PREVIEW_TEMPLATE, False)
     list_favorites(params)
 
-if mode == 'play':
+elif mode == 'play':
     play_stream(params)
